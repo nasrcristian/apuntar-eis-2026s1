8034abed53669c9d377a46b509dbd0420d7c669a
DamianVillalba <devillalba20@gmail.com>
2026-04-29 19:00:33 -0300

    fix: arreglo timezone psql y dejo seteado docker para la app en caso de ser necesario

--- backend/src/main/kotlin/ar/edu/unq/apuntar/ApuntarApplication.kt
+++ backend/src/main/kotlin/ar/edu/unq/apuntar/ApuntarApplication.kt
@@ -2,10 +2,13 @@ package ar.edu.unq.apuntar
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
 class ApuntarApplication
 
 fun main(args: Array<String>) {
+    //para el PSQL que no para de llorar por la zona horaria
+    TimeZone.setDefault(TimeZone.getTimeZone("America/Argentina/Buenos_Aires"))
 	runApplication<ApuntarApplication>(*args)
 }
