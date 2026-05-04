793037ee4e4d69a6d13a866bb4866c87515dbea2
DamianVillalba <devillalba20@gmail.com>
2026-05-04 17:20:04 -0300

    feat: lista de materiales page hecha

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { CssBaseline } from '@mui/material'
 import './App.css'
 
 import UploadPage from './pages/UploadPage'
+import MaterialListPage from './pages/MaterialListPage'
 
 function App() {
   return (
     <BrowserRouter>
       <CssBaseline />
       <Routes>
+        <Route path="/" element={<MaterialListPage />} />
         <Route path="/create" element={<UploadPage />} />
       </Routes>
     </BrowserRouter>
