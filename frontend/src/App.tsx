2e997d4117a66153f2092b398ec2799b52718b01
titoCo12 <joaquinfuentes170@gmail.com>
2026-05-04 19:21:11 -0300

    fix de router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { CssBaseline } from '@mui/material'
 import './App.css'
 
 import UploadPage from './pages/UploadPage'
 import MaterialPage from './pages/MaterialPage/MaterialPage'
 import MaterialListPage from './pages/MaterialListPage'
+import Register from './components/Register/Register'
 
 function App() {
   return (
-    <BrowserRouter>
-      <CssBaseline />
+   <>  
+    <CssBaseline />
       <Routes>
         <Route path="/" element={<MaterialListPage />} />
         <Route path="/create" element={<UploadPage />} />
         <Route path="/material/:id" element={<MaterialPage />}/>
+        <Route path="/register" element={<Register />} />
       </Routes>
-    </BrowserRouter>
+    </>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
-import RouterApp from './router.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <RouterApp />
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
