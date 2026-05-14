eccc243528e0262e1f3a5917a4818097cfc489ae
Tomas Fuentes <tomasrfuentes@gmail.com>
2026-05-13 22:34:24 -0300

    Eliminados warnings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,31 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { CssBaseline } from '@mui/material'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import "./App.css";
 
-import UploadPage from './pages/UploadPage'
-import MaterialPage from './pages/MaterialPage/MaterialPage'
-import MaterialListPage from './pages/MaterialListPage'
-import Register from './components/Register/Register'
-import LoginPage from './pages/auth/LoginPage'
-import HomePage from './pages/HomePage'
-import ForgotPasswordPage from './pages/auth/ForgotPasswordPage'
-import ResetPasswordPage from './pages/auth/ResetPasswordPage'
-import ProtectedRoute from './components/auth/ProtectedRoute'
+import UploadPage from "./pages/UploadPage";
+import MaterialPage from "./pages/MaterialPage/MaterialPage";
+import MaterialListPage from "./pages/MaterialListPage";
+import Register from "./components/Register/Register";
+import LoginPage from "./pages/auth/LoginPage";
+import HomePage from "./pages/HomePage";
+import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 function App() {
   return (
-   <>  
-    <CssBaseline />
+    <>
+      <CssBaseline />
       <Routes>
         <Route path="/" element={<MaterialListPage />} />
-        <Route path="/create" element={
-            <ProtectedRoute><UploadPage /></ProtectedRoute>
-        } />
+        <Route
+          path="/create"
+          element={
+            <ProtectedRoute>
+              <UploadPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/material/:id" element={<MaterialPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<LoginPage />} />
@@ -29,7 +34,7 @@ function App() {
         <Route path="/reset-password" element={<ResetPasswordPage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
