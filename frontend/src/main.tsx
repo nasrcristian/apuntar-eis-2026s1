4f72207167531f2296c85d554079b98b9e126176
Tomas Fuentes <tomasrfuentes@gmail.com>
2026-05-13 22:25:47 -0300

    Agregado notistack al projecto

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { SnackbarProvider } from "notistack";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <SnackbarProvider autoHideDuration={5000}>
+        <App />
+      </SnackbarProvider>
     </BrowserRouter>
   </StrictMode>,
-)
+);
