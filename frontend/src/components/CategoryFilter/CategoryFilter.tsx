25ed36d6a1924d437e0c917cf9e9a0c51dc755f9
Cristian Nasr <cristian.nasr@arba.gov.ar>
2026-05-18 00:20:00 -0300

    fix del default :*

--- frontend/src/components/CategoryFilter/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter/CategoryFilter.tsx
@@ -1,23 +1,35 @@
 import { TextField, MenuItem, Button, Box } from '@mui/material'
 import { categorias } from '../../constants/materialOptions'
 import './CategoryFilter.css'
+import { useState } from 'react'
 
 interface CategoryFilterProps {
   value: string
   onChange: (value: string) => void
 }
 
-export default function CategoryFilter({ value, onChange }: CategoryFilterProps) {
+export default function CategoryFilter({value, onChange}: CategoryFilterProps) {
+
+  const [label, setLabel] = useState("")
+
+  const defaultValue = "Ver todos";
+
+  function setValueAndLabel(value: string){
+    setLabel(value)
+    onChange(value === defaultValue? "" : value)
+  }
+
+
   return (
     <Box className="category-filter">
       <TextField
         select
         label="Categoría"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
+        value={label}
+        onChange={(e) => setValueAndLabel(e.target.value)}
         className="category-filter__select"
       >
-        <MenuItem key="" value="">Ver todos</MenuItem>
+        <MenuItem key="" value={defaultValue}>Ver todos</MenuItem>
         {categorias.map((c) => (
           <MenuItem key={c.value} value={c.value}>
             {c.label}
